919659dd007eca46c9b80fe33f2e0849562fb6d4
IlgazUygur1 <154981852+IlgazUygur1@users.noreply.github.com>
2024-10-16 23:02:18 +0300

    Initial Commit

---
+++ RPSTests/RPSTests.swift
@@ -0,0 +1,17 @@
+//
+//  RPSTests.swift
+//  RPSTests
+//
+//  Created by Ilgaz Uygur on 16.10.2024.
+//
+
+import Testing
+@testable import RPS
+
+struct RPSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
